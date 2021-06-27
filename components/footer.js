08e9a6e72352c6246f963c9cd4a009f6b555cd16
coteli <avsarcoteli@gmail.com>
2021-06-27 14:26:31 +0300

    social icons added

--- components/footer.js
+++ components/footer.js
@@ -1,27 +1,41 @@
 import Container from './container'
-import { EXAMPLE_PATH } from '../lib/constants'
+import { SocialIcon } from 'react-social-icons'
 
 export default function Footer() {
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
+        <div className="py-28 flex flex-col lg:flex-row lg:items-start">
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
+            Arabuluculuk ile ilgili bilgiler bulabileceğiniz bir blog.
           </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
-            <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
-            </a>
+          <div className="flex flex-row justify-center items-center lg:pl-4 lg:w-1/2 lg:space-x-5">
+              <div>
+                <SocialIcon
+              url="https://twitter.com/avsarcoteli"
+              className="mr-4"
+              target="_blank"
+              style={{ height: 35, width: 35 }}
+            />
+                </div>
+
+                <div>
+                <SocialIcon
+              url="https://www.youtube.com/channel/UCH5qLtQJercl1tURGPNCiHg"
+              className="mr-4"
+              target="_blank"
+              style={{ height: 35, width: 35 }}
+            />
+                </div>
+                <div>
+                <SocialIcon
+              url="https://www.linkedin.com/in/avsarcoteli/"
+              className="mr-4"
+              target="_blank"
+              style={{ height: 35, width: 35 }}
+            />
+                </div>
+            
           </div>
         </div>
       </Container>
